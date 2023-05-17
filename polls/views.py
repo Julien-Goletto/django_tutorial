c24f26917e69e69a633e125069807d32633e0477
Julien GOLETTO <juliengoletto@MacBook-Pro-de-Julien.local>
2023-05-17 11:03:08 +0200

    :sparkles: Adding fancy functions for loose coupling

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import Http404, HttpResponse
 from .models import Question
 from django.template import loader
@@ -12,14 +12,19 @@ def index(request):
     return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    # Traditionnal try catch structure
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+
+    # Handy built-in shortcut:
+    question = get_object_or_404(Question, pk=question_id)
+    # Another way to send back a HttpResponse with a shortcut
     return render(request, "polls/detail.html", { "question": question})
 
 def results(request, question_id):
-    response = "You're looking at the resuslts of question %s"
+    response = "You're looking at the results of question %s"
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
