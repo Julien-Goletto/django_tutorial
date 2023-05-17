414aec42020df05035459442ccbd27e89a263a40
Julien GOLETTO <juliengoletto@MacBook-Pro-de-Julien.local>
2023-05-17 11:41:12 +0200

    :sparkles: Vote mechanic achieved + race conditions trick

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import Http404, HttpResponse
-from .models import Question
+from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import Question, Choice
 from django.template import loader
+
+# Using F to avoid race conditions
+from django.db.models import F
 
 # Create your views here.
 
@@ -24,8 +28,28 @@ def detail(request, question_id):
     return render(request, "polls/detail.html", { "question": question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s"
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request, 
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_mesage": "You didn't select a choice.",
+            },
+        )
+    else:
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        # Introducing F for dealing with race conditions (in case of simultaneus POST requests)
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+    return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
